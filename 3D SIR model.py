3b6eb0edfc342d5cca1030d42d915e19a6293d05
Amina Rakhimbergenova <74665116+aminarhe@users.noreply.github.com>
2024-03-19 01:07:26 -0400

    Update 3D SIR model.py

--- 3D SIR model.py
+++ 3D SIR model.py
@@ -1,4 +1,18 @@
- # Import necessary libraries
+# In this project, I implemented a SIR (Susceptible-Infected-Recovered) model to simulate the spread of COVID-19 in 
+# Washington state during 2020. I used NumPy and Matplotlib libraries in Python, 
+# to perform numerical computations and create 2D and 3D visualizations.
+
+# The project involved researching and  defining the initial conditions for the population, setting the model parameters 
+# (infection and recovery rates), and implementing the Euler method to solve the SIR model equations numerically. The Euler
+# method computes the subsequent values of the susceptible, infected, and recovered populations at each time step based
+# on their values and derivatives at the previous step.
+
+# I created two types of visualizations to represent the simulation results. The 2D plot shows the evolution of the population
+# groups over time, while the 3D plot provides a unique perspective on the epidemic's dynamics by depicting the trajectories of
+# the populations in a three-dimensional space.
+
+
+# Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
